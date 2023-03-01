d443d5f4a6fe679be1893442162208f2c189b361
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-01 08:34:42 +0530

    Modified to Validate First Name program

--- RegularExpressions/Program.cs
+++ RegularExpressions/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regular Expression program");
+            Console.WriteLine("Please enter an option");
+            Console.WriteLine("1:Validate first name");
+            int option = Convert.ToInt32(Console.ReadLine());
+            RegexPattern regex = new RegexPattern();
+            switch (option)
+            {
+                case 1:
+                    regex.ValidateFirstName("Shivani");
+                    break;
+                default:
+                    Console.WriteLine("Please enter correct option");
+                    break;
+            }
         }
     }
 }
---
+++ RegularExpressions/RegexPattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressions
+{
+    public class RegexPattern
+    {
+        public void ValidateFirstName(string name)
+        {
+            string firstName = "^[A-z]{1}[a-z]{2,}$";
+            if (Regex.IsMatch(name, firstName))
+            {
+                Console.WriteLine("First name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First name is not valid");
+            }
+        }
+    }
+}
